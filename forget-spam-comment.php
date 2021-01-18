3f6133b11844d78212440d5de0525aa14920d3f0
Gulshan Kumar <dreamergulshan@gmail.com>
2021-01-18 22:12:40 +0530

    1.0.3
    String Localization, inline JS minification, other minor tweaks

--- forget-spam-comment.php
+++ forget-spam-comment.php
@@ -5,13 +5,12 @@
  * Description:       The ultimate solution to stop spam comments in default commenting system of WordPress.
  * Author:            Gulshan Kumar
  * Author URI:        https://www.gulshankumar.net/
- * Version:           1.0.2
+ * Version:           1.0.3
  * Text Domain:       forget-spam-comment
+ * Domain Path:       /languages
  */
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
+defined('ABSPATH') OR die();
 
 /* Register activation hook. */
 register_activation_hook( __FILE__, 'forget_spam_comment_activation_hook' );
@@ -26,10 +25,9 @@ function forget_spam_comment_activation_hook() {
     set_transient( 'forget-spam-comment-activation-notice', true, 5 );
 }
  
- 
 /* Add admin notice */
 add_action( 'admin_notices', 'forget_spam_comment_notice' );
- 
+
  
 /**
  * Admin Notice on Activation.
@@ -41,7 +39,7 @@ function forget_spam_comment_notice(){
     if( get_transient( 'forget-spam-comment-activation-notice' ) ){
         ?><style>div#message.updated{ display: none; }</style>
         <div class="updated notice is-dismissible">
-            <p>Thank you for using Forget Spam Comment. 😎 <strong>Please clear Page Cache</strong>.</p>
+          <p><?php _e( 'Thank you for using Forget Spam Comment. 😎 <strong>Please clear Page Cache</strong>', 'forget-spam-comment' ); ?></p>
         </div>
         <?php
         /* Delete transient, only display this notice once. */
@@ -53,8 +51,8 @@ function forget_spam_comment_notice(){
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'forget_spam_comment_add_action_links');
 function forget_spam_comment_add_action_links($links) {
     $plugin_shortcuts = array(
-        '<a rel="noopener" href="https://help.gulshankumar.net" target="_blank">Ask a Question</a>',
-        '<a rel="noopener" href="https://www.buymeacoffee.com/gulshan" target="_blank" style="color:#3db634;">Buy developer a coffee</a>'
+        '<a rel="noopener" href="https://help.gulshankumar.net" target="_blank">' . __('Ask a Question', 'forget-spam-comment') . '</a>',
+        '<a rel="noopener" href="https://www.buymeacoffee.com/gulshan" target="_blank" style="color:#080;">' . __('Buy developer a coffee', 'forget-spam-comment') . ' ☕' .'</a>'
     );
     return array_merge($links, $plugin_shortcuts);
 }
@@ -69,13 +67,13 @@ function forget_spam_comment_remove_comment_action_url( $defaults ) {
 // Step 2. As visitor scroll the web page, fix the broken action URL including md5 hash as Query String Parameter
 function forget_spam_comment_modify_action_url() {
 	if (is_singular() && comments_open()) {
-		echo "<script>\n";
+		echo "\n<script>";
 		// Most popular IDs are listed here for compatibility with all popular themes
-		echo "let commentForm = document.querySelector(\"#commentform, #ast-commentform, #ht-commentform\");\n";
-		echo "document.onscroll = function () {\n";
+		echo "let commentForm = document.querySelector(\"#commentform, #ast-commentform, #ht-commentform\");";
+		echo "document.onscroll = function () {";
 		$forget_spam_comment_unique_key  = md5($_SERVER['DOCUMENT_ROOT']); // Generate unique query string for each site
-		echo "commentForm.action = \"" . wp_make_link_relative(get_site_url()) . "/wp-comments-post.php?$forget_spam_comment_unique_key\";\n";
-		echo "};\n";
+		echo "commentForm.action = \"" . wp_make_link_relative(get_site_url()) . "/wp-comments-post.php?$forget_spam_comment_unique_key\";";
+		echo "};";
 		echo "</script>\n";
 	}
 }
@@ -86,7 +84,7 @@ function forget_spam_comment_block_response() {
 	header('HTTP/1.1 400 Bad Request');
 	header('Status: 400 Bad Request');
 	header('Connection: Close');
-	exit();
+	die();
 }
 
 // Step 4. Allow POST request for comment if md5 hash matches in Request Header as Query String Parameter
@@ -102,4 +100,4 @@ if ($forget_spam_comment_post_request_received) {
 		forget_spam_comment_block_response();
 	}
 }
-// Share your feedback at https://github.com/thegulshankumar/forget-spam-comment/
+// Share your feedback at https://wordpress.org/support/plugin/forget-spam-comment/reviews/#new-post
